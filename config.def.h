d3559f8c840c2611241a59374b27993b614f7c63
g <gerardaus@gmail.com>
2019-06-20 04:38:00 +0000

    brightness

--- config.def.h
+++ config.def.h
@@ -58,6 +58,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *brightness_up[] = {"dwm-brightness", "up", NULL };
+static const char *brightness_down[] = {"dwm-brightness", "down", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -84,6 +86,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+  { 0,                            0x1008ff02, spawn, {.v = brightness_up } },
+  { 0,                            0x1008ff03, spawn, {.v = brightness_down } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
