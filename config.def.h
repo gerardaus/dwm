0713f5248a42862b2ab9da42fc665b9cdd52c1d8
g <gerardaus@gmail.com>
2019-06-25 20:57:11 -0700

    increase border width; change border color; limit to 5 tags; use zsh with st; chromium shortcut; screen grab selection

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -13,14 +13,18 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_yellow[]      = "#b58900";
+
+// solarized colors
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_yellow  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5"  };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -58,12 +62,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "/bin/zsh", NULL };
 static const char *brightness_up[] = {"dwm-brightness", "up", NULL };
 static const char *brightness_down[] = {"dwm-brightness", "down", NULL };
 static const char *volume_up[] = {"amixer", "set", "Master", "5%+", NULL };
 static const char *volume_down[] = {"amixer", "set", "Master", "5%-", NULL };
 static const char *volume_mute[] = {"amixer", "-q", "sset", "Master", "toggle", NULL };
+static const char *chromiumcmd[] = {"/usr/bin/chromium", NULL };
+static const char *screengrab_select[] = {"/usr/bin/screengrab-select", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -109,6 +115,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_b,     spawn,          {.v = chromiumcmd } },
+	{ ControlMask,                  XK_Print, spawn,          {.v = screengrab_select } },
 };
 
 /* button definitions */
