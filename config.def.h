856d96907c05855dcc76d942d3873c957948a8f4
G <gerardaus@gmail.com>
2024-08-14 09:16:50 +1000

    increased gap size

--- config.def.h
+++ config.def.h
@@ -3,13 +3,13 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx = 2; /* border pixel of windows */
+static const unsigned int borderpx = 4; /* border pixel of windows */
 static const unsigned int gappx = 15;   /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"Inconsolata:size=22"};
-static const char dmenufont[] = "Inconsolata:size=22";
+static const char *fonts[] = {"Inconsolata:size=16"};
+static const char dmenufont[] = "Inconsolata:size=16";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
